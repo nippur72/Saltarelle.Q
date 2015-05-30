25adfa896d07bd9f0c919aa2fdae985cf49feac8
Antonino Porcino <nino.porcino@gmail.com>
2015-05-30 14:56:40 +0200

    Imlemented more Future methods

--- SaltarelleQ/Future.cs
+++ SaltarelleQ/Future.cs
@@ -24,6 +24,7 @@ public class FutureBase<T> : INotifyCompletion
 	#region async/await compiler interface
 
 	[InlineCode("{this}")] 
+	//[InlineCode("({this}.done(function(){{}},function(err){{throw err;}},function(){{}}))")] 
 	public extern FutureBase<T> GetAwaiter();
 	
 	public extern bool IsCompleted 
@@ -35,60 +36,67 @@ public class FutureBase<T> : INotifyCompletion
 	[InlineCode("{this}.inspect().value")]
 	public extern T GetResult();
 
-	[ScriptName("then")]
+	[ScriptName("done")]	
 	public extern void OnCompleted(Action continuation);
 
 	#endregion
 
-	// 0-p
-   [ScriptName("then")] public FutureBase<T> Then()                                                             { return null; }
+	#region Then
+   [ScriptName("then")] public extern FutureBase<T> Then(Action         successCallback);                                                              										 
+   [ScriptName("then")] public extern FutureBase<T> Then(Action         successCallback, Action         errorCallback);                                
+   [ScriptName("then")] public extern FutureBase<T> Then(Action         successCallback, Action<object> errorCallback);                                         							 
+   [ScriptName("then")] public extern FutureBase<T> Then(Action         successCallback, Action         errorCallback, Action         notifyCallback); 
+   [ScriptName("then")] public extern FutureBase<T> Then(Action         successCallback, Action         errorCallback, Action<object> notifyCallback); 
+   [ScriptName("then")] public extern FutureBase<T> Then(Action         successCallback, Action<object> errorCallback, Action         notifyCallback); 
+   [ScriptName("then")] public extern FutureBase<T> Then(Action         successCallback, Action<object> errorCallback, Action<object> notifyCallback); 
 
-   // 1-p
-   [ScriptName("then")] public FutureBase<T> Then(Action successCallback)                                       { return null; }
-   [ScriptName("then")] public FutureBase<T> Then(Action<object> successCallback)                               { return null; }
-   [ScriptName("then")] public FutureBase<T> Then(Func<FutureBase<T>> successCallback)                                 { return null; }
-   [ScriptName("then")] public FutureBase<T> Then(Func<object,FutureBase<T>> successCallback)                          { return null; }
+   [ScriptName("then")] public extern FutureBase<T> Then(Action<object> successCallback);                                                              
+   [ScriptName("then")] public extern FutureBase<T> Then(Action<object> successCallback, Action         errorCallback);                                
+   [ScriptName("then")] public extern FutureBase<T> Then(Action<object> successCallback, Action<object> errorCallback);                                
+   [ScriptName("then")] public extern FutureBase<T> Then(Action<object> successCallback, Action         errorCallback, Action         notifyCallback); 
+   [ScriptName("then")] public extern FutureBase<T> Then(Action<object> successCallback, Action         errorCallback, Action<object> notifyCallback); 
+   [ScriptName("then")] public extern FutureBase<T> Then(Action<object> successCallback, Action<object> errorCallback, Action         notifyCallback); 
+   [ScriptName("then")] public extern FutureBase<T> Then(Action<object> successCallback, Action<object> errorCallback, Action<object> notifyCallback); 
 
-   // 2-p
-   [ScriptName("then")] public FutureBase<T> Then(Action         successCallback, Action         errorCallback)  { return null; }
-   [ScriptName("then")] public FutureBase<T> Then(Action         successCallback, Action<object> errorCallback)  { return null; }
-   [ScriptName("then")] public FutureBase<T> Then(Action<object> successCallback, Action         errorCallback)  { return null; }
-   [ScriptName("then")] public FutureBase<T> Then(Action<object> successCallback, Action<object> errorCallback)  { return null; }
-      
-   // 3-p
-   [ScriptName("then")] public FutureBase<T> Then(Action         successCallback, Action         errorCallback, Action         notifyCallback) { return null; }
-   [ScriptName("then")] public FutureBase<T> Then(Action         successCallback, Action         errorCallback, Action<object> notifyCallback) { return null; }
-   [ScriptName("then")] public FutureBase<T> Then(Action         successCallback, Action<object> errorCallback, Action         notifyCallback) { return null; }
-   [ScriptName("then")] public FutureBase<T> Then(Action         successCallback, Action<object> errorCallback, Action<object> notifyCallback) { return null; }
-   [ScriptName("then")] public FutureBase<T> Then(Action<object> successCallback, Action         errorCallback, Action         notifyCallback) { return null; }
-   [ScriptName("then")] public FutureBase<T> Then(Action<object> successCallback, Action         errorCallback, Action<object> notifyCallback) { return null; }
-   [ScriptName("then")] public FutureBase<T> Then(Action<object> successCallback, Action<object> errorCallback, Action         notifyCallback) { return null; }
-   [ScriptName("then")] public FutureBase<T> Then(Action<object> successCallback, Action<object> errorCallback, Action<object> notifyCallback) { return null; }
+   [ScriptName("then")] public extern FutureBase<T> Then(Func<FutureBase<T>> successCallback);                                                              										 
+   [ScriptName("then")] public extern FutureBase<T> Then(Func<FutureBase<T>> successCallback, Action         errorCallback);                                
+   [ScriptName("then")] public extern FutureBase<T> Then(Func<FutureBase<T>> successCallback, Action<object> errorCallback);                                         							 
+   [ScriptName("then")] public extern FutureBase<T> Then(Func<FutureBase<T>> successCallback, Action         errorCallback, Action         notifyCallback); 
+   [ScriptName("then")] public extern FutureBase<T> Then(Func<FutureBase<T>> successCallback, Action         errorCallback, Action<object> notifyCallback); 
+   [ScriptName("then")] public extern FutureBase<T> Then(Func<FutureBase<T>> successCallback, Action<object> errorCallback, Action         notifyCallback); 
+   [ScriptName("then")] public extern FutureBase<T> Then(Func<FutureBase<T>> successCallback, Action<object> errorCallback, Action<object> notifyCallback); 
 
-   //[ScriptName("catch")] public Promise Catch<TResult>(Action<object> onRejected: (reason: any) => IHttpPromise<TResult>){ return null; }
-   //[ScriptName("catch")] public Promise Catch<TResult>(onRejected: (reason: any) => IPromise<TResult>)    { return null; }
-   //[ScriptName("catch")] public Promise Catch<TResult>(onRejected: (reason: any) => TResult)              { return null; }
+   [ScriptName("then")] public extern FutureBase<T> Then(Func<object,FutureBase<T>> successCallback);                                                              
+   [ScriptName("then")] public extern FutureBase<T> Then(Func<object,FutureBase<T>> successCallback, Action         errorCallback);                                
+   [ScriptName("then")] public extern FutureBase<T> Then(Func<object,FutureBase<T>> successCallback, Action<object> errorCallback);                                
+   [ScriptName("then")] public extern FutureBase<T> Then(Func<object,FutureBase<T>> successCallback, Action         errorCallback, Action         notifyCallback); 
+   [ScriptName("then")] public extern FutureBase<T> Then(Func<object,FutureBase<T>> successCallback, Action         errorCallback, Action<object> notifyCallback); 
+   [ScriptName("then")] public extern FutureBase<T> Then(Func<object,FutureBase<T>> successCallback, Action<object> errorCallback, Action         notifyCallback); 
+   [ScriptName("then")] public extern FutureBase<T> Then(Func<object,FutureBase<T>> successCallback, Action<object> errorCallback, Action<object> notifyCallback); 
+	#endregion
 
-   [ScriptName("finally")] public FutureBase<T> Finally(Action FinallyCallback) { return null; }
-	
-	/*
-   [InlineCode(@"(function(fn){{{this}.then(     function(response){{fn(response);}});return {this};}})({func})")] public FutureBase Success<T>(Action<T> func) { return null; }
-   [InlineCode(@"(function(fn){{{this}.then(null,function(response){{fn(response);}});return {this};}})({func})")] public FutureBase Error  <T>(Action<T> func) { return null; }
+	#region Done
+   [ScriptName("done")] public extern void Done(Action         successCallback);                                                              
+   [ScriptName("done")] public extern void Done(Action<object> successCallback);                                                              										 
+   [ScriptName("done")] public extern void Done(Action         successCallback, Action         errorCallback);                                
+   [ScriptName("done")] public extern void Done(Action         successCallback, Action<object> errorCallback);                                
+   [ScriptName("done")] public extern void Done(Action<object> successCallback, Action         errorCallback);                                
+   [ScriptName("done")] public extern void Done(Action<object> successCallback, Action<object> errorCallback);                                         							 
+   [ScriptName("done")] public extern void Done(Action         successCallback, Action         errorCallback, Action         notifyCallback); 
+   [ScriptName("done")] public extern void Done(Action         successCallback, Action         errorCallback, Action<object> notifyCallback); 
+   [ScriptName("done")] public extern void Done(Action         successCallback, Action<object> errorCallback, Action         notifyCallback); 
+   [ScriptName("done")] public extern void Done(Action         successCallback, Action<object> errorCallback, Action<object> notifyCallback); 
+   [ScriptName("done")] public extern void Done(Action<object> successCallback, Action         errorCallback, Action         notifyCallback); 
+   [ScriptName("done")] public extern void Done(Action<object> successCallback, Action         errorCallback, Action<object> notifyCallback); 
+   [ScriptName("done")] public extern void Done(Action<object> successCallback, Action<object> errorCallback, Action         notifyCallback); 
+   [ScriptName("done")] public extern void Done(Action<object> successCallback, Action<object> errorCallback, Action<object> notifyCallback); 
+	#endregion
 
-   [InlineCode(@"(function(fn){{{this}.then(     function(response){{fn(response);}});return {this};}})({func})")] public FutureBase Success(Action<object> func) { return null; }
-   [InlineCode(@"(function(fn){{{this}.then(null,function(response){{fn(response);}});return {this};}})({func})")] public FutureBase Error  (Action<object> func) { return null; }
-      
-   [InlineCode(@"(function(fn){{{this}.then(     function(response){{fn();}});return {this};}})({func})")] public FutureBase Success(Action func) { return null; }
-   [InlineCode(@"(function(fn){{{this}.then(null,function(response){{fn();}});return {this};}})({func})")] public FutureBase Error  (Action func) { return null; }      
+	#region Catch
+   [ScriptName("catch")] public extern FutureBase<T> Catch(Action         errorCallback);
+   [ScriptName("catch")] public extern FutureBase<T> Catch(Action<object> errorCallback);
+	#endregion Catch
 
-   [InlineCode(@"(function(fn){{{this}.then(     function(response){{fn(response);}});return {this};}})({func})")] public FutureBase Success<T>(Func<T,FutureBase> func) { return null; }
-   [InlineCode(@"(function(fn){{{this}.then(null,function(response){{fn(response);}});return {this};}})({func})")] public FutureBase Error  <T>(Func<T,FutureBase> func) { return null; }
-
-   [InlineCode(@"(function(fn){{{this}.then(     function(response){{fn(response);}});return {this};}})({func})")] public FutureBase Success(Func<object,FutureBase> func) { return null; }
-   [InlineCode(@"(function(fn){{{this}.then(null,function(response){{fn(response);}});return {this};}})({func})")] public FutureBase Error  (Func<object,FutureBase> func) { return null; }
-      
-   [InlineCode(@"(function(fn){{{this}.then(     function(response){{fn();}});return {this};}})({func})")] public FutureBase Success(Func<FutureBase> func) { return null; }
-   [InlineCode(@"(function(fn){{{this}.then(null,function(response){{fn();}});return {this};}})({func})")] public FutureBase Error  (Func<FutureBase> func) { return null; }      
-	*/
+   [ScriptName("finally")] public extern FutureBase<T> Finally(Action FinallyCallback);	
 }                         
 
